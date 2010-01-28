454e717f9acfde5d7e1bc25dd16539960c4ac6d4
Andrew Clarke <andrew@glydetechnology.com>
2010-01-28 16:44:08 +0000

    Added extra comments and link to github wiki for documentation

--- xhtml/config/xhtml.php
+++ xhtml/config/xhtml.php
@@ -2,32 +2,60 @@
 	
 return array(
 
-	// Application defaults
+	// Xhtml defaults
 	'default' => array(
-		'doctype'		=> xhtml::DOCTYPE_XHTML_1_0_STRICT,
+	
+		// Doctype definition - should be self explanatory
+		// One of the following constants:
+		// 		Xhtml::DOCTYPE_HTML_4_01_STRICT
+		// 		Xhtml::DOCTYPE_HTML_4_01_FRAMESET
+		// 		Xhtml::DOCTYPE_HTML_4_01_TRANSITIONAL
+		// 		Xhtml::DOCTYPE_XHTML_1_0_STRICT
+		// 		Xhtml::DOCTYPE_XHTML_1_0_FRAMESET
+		// 		Xhtml::DOCTYPE_XHTML_1_0_TRANSITIONAL
+		// 		Xhtml::DOCTYPE_XHTML_1_1_STRICT
+		'doctype'		=> Xhtml::DOCTYPE_XHTML_1_0_STRICT,
+		
+		// Language code - If not specified this is autodetected from i18n module
 		//'langcode'	=> 'en-fr',
+		
+		// Additional attributes for the html tag
 		//'htmlatts'	=> array(),
+		
+		// Head defaults
 		'head' => array(
+		
+			// Content of the title tag
 			'title'		=> 'default title',
+			
+			// Meta tags
 			'meta' => array(
-				'keywords'		=> 'keywords',
-				'description'	=> 'description of site',
+				//'keywords'		=> 'keywords',
+				//'description'	=> 'description of site',
 			),
+			
+			// Link tags
 			'links' => array(
-				'home' => array(
-					'href'	=> '/',
-					'rel'	=> 'home',
-					'title'	=> 'Home Page',
-				),
+				//'home' => array(
+				//	'href'	=> '/',
+				//	'rel'	=> 'home',
+				//	'title'	=> 'Home Page',
+				//),
 			),
+			
+			// Styles
 			'styles' => array(
-				'main styles' => 'styles.css',
+				//'main styles' => 'styles.css',
 			),
+			
+			// Script tags
 			'scripts' => array(
-				'main scripts' => 'scripts.js',
+				//'main scripts' => 'scripts.js',
 			),
+			
+			// Inline scripts
 			'codes' => array(
-				'default code' => 'var $i=0;',
+				//'default code' => 'var $i=0;',
 			),
 		),
 	),
